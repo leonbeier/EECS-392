0f5f499cb595a8f7bcc8bea95b760f7b22c1abb7
Tigist Diriba <tgdiriba@gmail.com>
2015-04-26 16:33:41 -0500

    fifo and uart work

---
+++ src/fifo.vhdl
@@ -0,0 +1,78 @@
+library IEEE;
+
+use IEEE.std_logic_1164.all;
+use WORK.tracker_constants.all;
+
+entity fifo is
+  generic(
+    constant BUFFER_SIZE : integer := 100;
+    constant DATA_WIDTH : integer := 32
+  );
+  
+  port(
+    signal read_clk : in std_logic;
+    signal write_clk : in std_logic;
+    signal reset : in std_logic;
+    signal read_en : in std_logic;
+    signal write_en : in std_logic;
+    signal data_in : in std_logic_vector((DATA_WIDTH-1) downto 0);
+    signal data_out : out std_logic_vector((DATA_WIDTH-1) downto 0);
+    signal full : out std_logic;
+    signal empty : out std_logic;
+  );
+end entity fifo;
+  signal head : integer := 0;
+  signal head_update : integer;
+  signal tail : integer := 0;
+  signal tail_update : integer;
+  
+  type buffer_t is array((BUFFER_SIZE-1) downto 0)  of std_logic_vector((DATA_WIDTH-1) downto 0);
+  signal fifo_buffer : buffer_t;
+architecture fifo of fifo is
+
+  buffer_writer: process(write_clk) begin
+    if(rising_edge(write_clk)) then
+      if(write_en = '1' and full = '0') then
+        fifo_buffer(tail) <= data_in;
+      end if;
+    end if;
+  end process buffer_writer;
+  
+  buffer_tail process(write_clk, reset) begin
+    if(reset = '1') then
+      tail <= 0;
+    elsif(rising_edge(write_clk)) then
+      tail <= tail_update;
+    end if;
+  end process buffer_tail;
+  
+  tail_update <= tail+1 when (write_en = '1' and full = '0' and tail /= BUFFER_SIZE-1) else
+                 0 when (write_en = '1' and full = '0' and tail = BUFFER_SIZE-1) else
+                 tail;
+  
+  full <= '0' when (head < BUFFER_SIZE-2 and head /= tail+2) else
+          '0' when (head = BUFFER_SIZE-2 and head /= 0) else
+          '0' when (head = BUFFER_SIZE-1 and head /= 1) else
+          '1';
+  
+  buffer_reader: process(read_clk) ebgin
+    if(rising_edge(read_clk) then
+      data_out <= fifo_buffer(head);
+    end if;
+  end process buffer_reader;
+
+  buffer_head: process(read_clk, reset) begin
+    if(reset = '1') then
+      head <= 0;
+    elsif(rising_edge(read_clk)) then
+      head <= head_update;
+    end if;
+  end process buffer_head;
+  
+  head_update <= head+1 when (read_en = '1' and empty = '0' and head /= BUFFER_SIZE-1) else
+                 0 when (read_en = '1' and empty = '0' and head = BUFFER_SIZE-1) else
+                 head;
+  
+  empty <= '0' when (head /= tail) else '1';
+ 
+end architecture;
--- src/uart.vhdl
+++ src/uart.vhdl
@@ -3,10 +3,12 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use WORK.tracker_constants.all;
+use WORK.fifo.all;
 
 entity uart is
   port (
-    baud_rate : in integer = DEFAULT_BAUD_RATE;
+    reset : in std_logic;
+    baud_rate : in integer := DEFAULT_BAUD_RATE;
     rx : in std_logic,
     tx : in std_logic,
   );
@@ -19,19 +21,21 @@ architecture uart of uart is
   
   -- uart control
   signal uart_rx_state : uart_state;
-  signal uart_rx_next : uart_state
+  signal uart_rx_next : uart_state;
+  signal uart_rx_fifo : fifo;
   signal uart_tx_state : uart_state;
   signal uart_tx_next : uart_state;
+  signal uart_tx_fifo : fifo;
 begin
 
-  uart_clk_trigger : process(CLOCK_50, RESET_N) is
+  uart_rx_trigger : process(CLOCK_50, RESET_N) is
     -- check the baud rate and set the counter
     variable internal_count : integer := 0;
     variable bit_count : unsigned integer := 0;
     variable bit_buffer : std_logic_vector(7 downto 0);
   begin
     if(internal_count = counter_clk) then
-      case(uart_state) is
+      case(uart_rx_state) is
         -- check for a start bit
         when INIT =>
           if(rx = '1') then
@@ -45,14 +49,20 @@ begin
             uart_rx_next <= STOP;
           end if;
         when STOP =>
-          -- check if the next one or two bits are high
-          --  if they are high then write the buffer to the fifo
+          -- check if the next bit is high
+          --  if it is then write the buffer to the fifo
           if(rx = '1') then
             -- load data into byte fifo
             -- TODO
           end if;
     end if;
-  end process buffer_proc;
+  end process uart_rx_trigger;
+  
+  uart_tx_trigger: process() is
+    -- Data
+  begin
+    -- Write out the tx fifo until empty
+  end process uart_tx_trigger;
   
   uart_state_machine: process()
     
