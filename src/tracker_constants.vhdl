f66820dac4fd27077120006d8a455e4684962b7e
Tigist Diriba <tgdiriba@gmail.com>
2015-05-26 16:00:51 -0500

    Modifying the sine look-up table testbench

--- src/tracker_constants.vhdl
+++ src/tracker_constants.vhdl
@@ -1,16 +1,14 @@
 library IEEE;
+library IEEE_PROPOSED;
     
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+use IEEE_PROPOSED.fixed_pkg.all;
 
 package tracker_constants is
   -- 50MHz clock with a default baud rate of 9600 (bits/second)
   constant CLOCK_FREQUENCY : integer := 50000000;
   constant DEFAULT_BAUD_RATE : natural := 25000000;   -- 9600
-  
-  -- math constants
-  constant PI : integer := 3141592;
-  constant PI_DIV : integer := 1000000;
   
   -- Image constants
   constant IMAGE_WIDTH : natural := 640;
@@ -26,4 +24,13 @@ package tracker_constants is
   
   -- adv7180 constants
   constant ADV7180_LINES : natural := 720;
+  
+  -- ROM constants
+  constant ROM_DATA_WIDTH : natural := 10;
+  
+  -- fixed point math constants
+  constant PI : ufixed(11 downto -20) := 3.14159265358979323846;
+  -- dependent on the length of the sine lut in rom
+  constant SINE_LUT_RESOLUTION : ufixed(11 downto -20) := 0.00153248422126331377;
+  constant SINE_LUT_LENGTH : natural := 1024;
 end package tracker_constants;
