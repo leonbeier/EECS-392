3792fba231ed5a0b25ed9c990d4edd6116a5ac31
Brady Lee <bradylee2016@u.northwestern.edu>
2015-06-05 19:02:27 -0500

    output real & filtered, but no centroid

---
+++ src/video_out/video_out_constants.vhd
@@ -0,0 +1,113 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package video_out_constants is
+
+  constant FULL_WIDTH : natural := 640;
+  constant FULL_HEIGHT : natural := 480;
+  constant IMG_WIDTH : natural := FULL_WIDTH / 2;
+  constant IMG_HEIGHT : natural := FULL_HEIGHT / 2;
+  constant IMG_SIZE : natural := IMG_WIDTH * IMG_HEIGHT;
+  constant YCC_RAM_SIZE : natural := IMG_SIZE / 2;
+  constant BW_BUFFER_WIDTH : natural := 8;
+  constant BW_RAM_SIZE : natural := IMG_SIZE / BW_BUFFER_WIDTH;
+  constant PIXEL_WIDTH : natural := 24;
+  constant YCC_WIDTH : natural := 32;
+  constant SAMPLE_WIDTH : natural := 8;
+  
+component vga is
+	port(
+			clk, reset	: in std_logic;
+			pixel : in std_logic_vector(23 downto 0);
+			pixel_clock_out	: out std_logic;												
+			pixel_row, pixel_col	: out std_logic_vector(9 downto 0);
+			horiz_sync_out, vert_sync_out : out std_logic; 
+			vga_blank : out std_logic;
+			red, green, blue : out std_logic_vector(7 downto 0)
+		);					    		
+end component vga;
+
+component sram is
+  generic(
+    RAM_SIZE: natural := 128;
+    DATA_WIDTH : natural := 32 
+  );
+  port(
+    clk: in std_logic;
+    we : in std_logic;
+    write_addr: in natural range 0 to RAM_SIZE-1;
+    data_in: in std_logic_vector(DATA_WIDTH-1 downto 0);
+    read_addr: in natural range 0 to RAM_SIZE-1;
+    data_out: out std_logic_vector(DATA_WIDTH-1 downto 0)
+  );
+end component sram;
+
+component ycc2rgb is
+  port (
+    clk : in std_logic;
+    y, cb, cr : in std_logic_vector(7 downto 0);
+    r, g, b : out std_logic_vector(7 downto 0)
+  ); 
+end component ycc2rgb;
+
+component ycc_filter is
+  port(
+    clk : in std_logic;    
+    y : in natural;
+    cb : in natural;
+    cr : in natural;
+    y_key : in natural;
+    cb_key : in natural;
+    cr_key : in natural;
+    result : out std_logic
+  );
+end component ycc_filter;
+
+component pixel_address is
+  port(
+    pixel_row : in natural;
+    pixel_col : in natural;
+    
+    ycc_read_addr : out natural;
+    ycc_pixel_sel : out std_logic;
+    bw_read_addr : out natural;
+    bw_pixel_sel : out natural
+  );
+end component;
+
+component fifo is
+  generic(
+    constant BUFFER_SIZE : natural := 128;
+    constant DATA_WIDTH : natural := 8
+  );
+  port(
+    signal read_clk : in std_logic;
+    signal write_clk : in std_logic;
+    signal reset : in std_logic;
+    signal read_en : in std_logic;
+    signal write_en : in std_logic;
+    signal data_in : in std_logic_vector((DATA_WIDTH-1) downto 0);
+    signal data_out : out std_logic_vector((DATA_WIDTH-1) downto 0);
+    signal full : out std_logic;
+    signal empty : out std_logic
+  );
+end component fifo;
+
+component centroid is
+  generic(
+    ROWS : natural := 240;
+    COLS : natural := 320
+  ); 
+  port(
+    clk : in std_logic;
+    reset : in std_logic;
+    pixel_buffer : in std_logic_vector(7 downto 0);
+    
+    center_row : out natural;
+    center_col : out natural
+    --complete : out std_logic
+  );
+end component centroid;
+  
+end package;
--- src/video_out/video_out_tb.vhd
+++ src/video_out/video_out_tb.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity video_out_tb is
 end entity;
@@ -9,6 +10,7 @@ architecture test of video_out_tb is
 component video_out is
 	port(
 			clk : in std_logic;
+			clk_27 : in std_logic;
 			reset : in std_logic;
 			
 			--VGA 
@@ -18,34 +20,66 @@ component video_out is
 		);
 end component video_out;
 
-signal clk : std_logic;
+signal clk, clk_27 : std_logic;
 signal reset : std_logic; 
 signal red, green, blue : std_logic_vector(7 downto 0); 
 signal h_sync, v_sync, blank, vga_clk	: std_logic;
 
+constant CLK_FRQ : natural := 50000000;
+constant CLK_PER : time := 1 sec / CLK_FRQ;
+constant half_period : time := CLK_PER / 2;
+constant CLK_FRQ_27 : natural := 27000000;
+constant CLK_PER_27 : time := 1 sec / CLK_FRQ_27;
+constant half_period_27 : time := CLK_PER_27 / 2;
+
+constant CYCLE_MAX : natural := 4000000;
+signal done : std_logic := '0';
+
 begin
   
-    test_video : video_out port map(clk, reset, red, green, blue, h_sync, v_sync, blank, vga_clk);
-  
+    test_video : video_out port map(clk, clk_27, reset, red, green, blue, h_sync, v_sync, blank, vga_clk);
+
+  process
+  begin
+      reset <= '0';
+      wait for CLK_PER;
+      reset <= '1';
+      wait;
+  end process;
+
   process
   begin
     
     clk <= '1';
-    reset <= '1';
-    wait for 5 ns;
+    wait for half_period;
     clk <= '0';
-    wait for 5 ns;
-    reset <= '0';
+    wait for half_period;
   
-    for i in 1 to 2000000 loop
-      clk <= '1';
-      wait for 5 ns;
-      clk <= '0';
-      wait for 5 ns;
+    for i in 1 to CYCLE_MAX loop
+      clk <= not clk;
+      wait for half_period;
+      clk <= not clk;
+      wait for half_period;
     end loop;
     
-    wait for 5 ns;
+    done <= '1';
+    
+    wait for half_period;
     wait;
+  
+  end process;
+  
+  process
+  begin
+    
+    if (done = '1') then
+      wait;
+    end if;
+    
+    clk_27 <= '1';
+    wait for half_period_27;
+    clk_27 <= '0';
+    wait for half_period_27;
   
   end process;
   
