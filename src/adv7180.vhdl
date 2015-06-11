31348a10ea1c25871e4918af220abcc5935c3845
Tigist Diriba <tgdiriba@gmail.com>
2015-06-11 00:26:04 -0500

    Updated adv7180 for better setup and hold times

--- src/adv7180.vhdl
+++ src/adv7180.vhdl
@@ -40,7 +40,7 @@ architecture adv7180 of adv7180 is
 
 begin
     
-  ram_clk <= td_clk27;
+  ram_clk <= not td_clk27;
   ram_write_addr <= data_address;
   
   adv7180_decoder: process(td_data, td_clk27, td_hs, td_vs, td_reset) is
@@ -53,14 +53,14 @@ begin
   begin
     if(td_reset = '0') then
       state <= VS_RESET;
-    elsif(falling_edge(td_clk27)) then
+    elsif(rising_edge(td_clk27)) then
       case(state) is
         when VS_RESET | HS_RESET =>
           -- HS_RESET and VS_RESET
           ram_we <= '0';
           buffer_index := 0;
           data_buffer := (others => '0');
-          clock_count := 4;
+          clock_count := 5;
 
           -- HS_RESET has a lower priority
           decimation_count_cols := 0;
@@ -128,44 +128,56 @@ begin
   
   -- Generates flag required for HS_RESET state transition in main state machine
   hs_manager:  process(td_data, td_clk27, td_hs, td_vs, td_reset) is
-    variable hs_idle : boolean := true;
+    type hs_state is (waiting, idle, transition);
+    variable state : hs_state := idle;
   begin
     if(td_reset = '0') then
-      hs_idle := true;
+      state := idle;
       hs_flag <= '0';
-    elsif(rising_edge(td_clk27)) then
+    elsif(falling_edge(td_clk27)) then
       hs_flag <= '0';
-      if(hs_idle) then
+      if(state = waiting) then
+        if(td_hs = '0') then
+          state := idle;
+        end if;
+      elsif(state = idle) then
         if(td_hs = '1') then
           hs_flag <= '1';
-          hs_idle := false;
+          state := transition;
         end if;
+      elsif(state = transition) then
+        hs_flag <= '1';
+        state := waiting;
       else
-        if(td_hs = '0') then
-          hs_idle := true;
-        end if;
+        state := waiting;
       end if;
     end if;
   end process hs_manager;
   
   -- Generates flag required for VS_RESET state transition in main state machine
   vs_manager: process(td_data, td_clk27, td_hs, td_vs, td_reset) is
-    variable vs_idle : boolean := true;
+    type vs_state is (waiting, idle, transition);
+    variable state : vs_state := idle;
   begin
     if(td_reset = '0') then
-      vs_idle := true;
+      state := idle;
       vs_flag <= '0';
-    elsif(rising_edge(td_clk27)) then
+    elsif(falling_edge(td_clk27)) then
       vs_flag <= '0';
-      if(vs_idle) then
+      if(state = waiting) then
+        if(td_vs = '0') then
+          state := idle;
+        end if;
+      elsif(state = idle) then
         if(td_vs = '1') then
           vs_flag <= '1';
-          vs_idle := false;
+          state := transition;
         end if;
+      elsif(state = transition) then
+        vs_flag <= '1';
+        state := waiting;
       else
-        if(td_vs = '0') then
-          vs_idle := true;
-        end if;
+        state := waiting;
       end if;
     end if;
   end process vs_manager;
