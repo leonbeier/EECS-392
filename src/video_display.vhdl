7a540d30d6d61ebd347e1ecd79963954a1a13c7a
Tigist Diriba <tgdiriba@gmail.com>
2015-06-10 23:21:27 -0500

    LED test

--- src/video_display.vhdl
+++ src/video_display.vhdl
@@ -280,7 +280,7 @@ begin
           end if;
         when DONE_CONFIG =>
           i2c_status_led <= '1';
-          i2c_config_state <= DONE_CONFIG;
+          i2c_config_state <= INIT_CONFIG;
         when others =>
           i2c_config_state <= INIT_CONFIG;
       end case;
@@ -306,7 +306,7 @@ begin
   
   gpio_i2c_clk <= i2c_clk;
   gpio_i2c_data <= i2c_data;
-  i2c_availability <= i2c_available;
+  i2c_availability <= '1'; -- i2c_available;
   
   i2c_state_count <= "0000" when (i2c_config_state = INIT_CONFIG) else
                      "0001" when (i2c_config_state = I2C_ADDR_CONFIG) else
